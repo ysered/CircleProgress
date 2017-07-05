663b27aa811263a714dcdfe863b93d43b96acf21
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-07-05 16:09:54 +0300

    Set UI.

--- app/src/main/java/com/ysered/circleprogress/MainActivity.kt
+++ app/src/main/java/com/ysered/circleprogress/MainActivity.kt
@@ -2,11 +2,34 @@ package com.ysered.circleprogress
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import com.ysered.circleprogress.view.CircleProgressView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progressBar = findViewById(R.id.progressBar) as CircleProgressView
+        val progressSeekBar = findViewById(R.id.seekBar) as SeekBar
+        progressBar.setProgress(progressSeekBar.progress)
+
+        progressSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                progressBar.setProgress(progress)
+                if (progress < 40) {
+                    progressBar.setText("Good")
+                } else if (progress in 40..70) {
+                    progressBar.setText("Normal")
+                } else {
+                    progressBar.setText("Bad")
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
